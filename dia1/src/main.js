d198111ed3b92004d17b91766c7f066d1df2fae2
Thiago Malaquias <tammalaquias@gmail.com>
2021-11-06 12:13:01 -0300

    feature: abrir e fechar div

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -4,3 +4,13 @@ document.querySelector('.app').innerHTML = `
   <h1>B. Academy</h1>
   <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
 `
+const link = document.querySelector('[data-js="link"]')
+link.addEventListener('click', (event) => {
+  event.preventDefault()
+  const display = document.querySelector('.app').style.display
+  if(display == "block" || display == ""){
+    document.querySelector('.app').style.display = "none"
+  }else{
+    document.querySelector('.app').style.display = "block"
+  }
+}, false)
